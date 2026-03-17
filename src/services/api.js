f54f6fa517fdbf1e4f1e868160cc3382fa7b51d5
gaddi245 <steevassih@gmail.com>
2026-03-17 23:27:35 +0100

    m

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://projet-biomim-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://biomimbackend.onrender.com/api",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
